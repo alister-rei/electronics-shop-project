6a9b0b2f11c469faf04cb8dd4eeaf4d00dc73998
Хадков_Алексей <lhadkov@mail.ru>
2023-07-16 20:38:25 +0300

    изменение тестов согласно заданию все тесты в 1 файле

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,32 @@
 from src.item import Item
 import os
 
+
+# Тесты для проверки класса для ДЗ 13.1 homework_1
+def test_item():
+    item1 = Item("Смартфон", 10000, 10)
+
+    # Тест атрибутов экземпляра класса
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 10
+
+    # Тест методов класса
+    assert item1.calculate_total_price() == 100000
+    # Тест атрибута класса для метода
+    item2 = Item("Samsung", 15000, 5)
+    Item.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 5000
+    assert item2.price == 15000
+    assert Item.all == [item1, item2]
+
+
+"""
+Тесты для ДЗ 13.2 homework_2
+"""
+
+
 # TestCase_1 : property , name.getter name.setter
 def test_property_name():
     item1 = Item("samsung", 10000, 5)
@@ -9,6 +35,7 @@ def test_property_name():
     assert item1.name == "macbook"
     item1.name = "samsung galaxy gt one"
     assert item1.name == "samsung ga"
+
 
 # TestCase_2 : classmethod instantiate_from_csv
 # Only from Pytest in terminal from file ...\electronics-shop-project
@@ -21,9 +48,9 @@ def test_instantiate_from_csv():
     assert Item.all[3].name == "Мышка"
     assert Item.all[4].name == "Клавиатура"
 
+
 # TestCase_3 : staticmethod string_to_number
 def test_string_to_number():
     assert Item.string_to_number("4") == 4
     assert Item.string_to_number("4.0") == 4
     assert Item.string_to_number("4.9") == 4
-
