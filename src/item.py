6468d0e26d39ebb59f640ca53260f226ef837d4e
Хадков_Алексей <lhadkov@mail.ru>
2023-07-28 19:49:47 +0300

    create method add in class Item

--- src/item.py
+++ src/item.py
@@ -3,33 +3,37 @@ import os
 
 
 class Item:
-    """
+    '''
     Класс для представления товара в магазине.
-    """
+    '''
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
+        '''
         Создание экземпляра класса item.
 
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
-        """
+        '''
         self.name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         Item.all.append(self)
 
-    def __repr__(self):
-        ''' Данные экземпляра класса для разработчика '''
-        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+    def __repr__(self) -> str:
+        '''
+        Данные экземпляра класса для разработчика
+        '''
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
-    def __str__(self):
-        """ Данные по экземпляру класса для пользователя """
-        return f"{self.__name}"
+    def __str__(self) -> str:
+        '''
+        Данные по экземпляру класса для пользователя
+        '''
+        return f"{self.name}"
 
     @property
     def name(self) -> str:
@@ -48,6 +52,30 @@ class Item:
         else:
             self.__name = name
 
+    @property
+    def price(self) -> float:
+        '''
+        геттер метод для получения приватного атрибута __price
+        '''
+        return self.__price
+
+    @property
+    def quantity(self) -> int:
+        '''
+        геттер метод для получения приватного атрибута __quantity
+        '''
+        return self.__quantity
+
+    @quantity.setter
+    def quantity(self, quantity: int) -> None:
+        '''
+        сеттер метод для перезаписи атрибута __quantity
+        '''
+        if type(quantity) == int and quantity >= 0:
+            self.__quantity = quantity
+        else:
+            raise ValueError('Количество физических товаров должно быть целым числом больше нуля.')
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -60,10 +88,18 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.pay_rate * self.price
+        self.__price = self.pay_rate * self.price
+
+    def __add__(self, other) -> ValueError | int:
+        '''
+        Складывает объекты класса "Item" и дочерние от него
+        '''
+        if not isinstance(other, self.__class__):
+            raise ValueError('Складывать можно только объекты "Item" и дочерние от них.')
+        return self.quantity + other.quantity
 
     @classmethod
-    def instantiate_from_csv(cls, path=os.path.join(os.path.dirname(__file__), "..", "src", "items.csv")):
+    def instantiate_from_csv(cls, path=os.path.join(os.path.dirname(__file__), "..", "src", "items.csv")) -> None:
         '''
         открывает файт csv и на основе его данных создает экземпляры класса, которые записывает в список all
         '''
@@ -78,8 +114,8 @@ class Item:
                     cls(name, price, quantity)
 
     @staticmethod
-    def string_to_number(value):
+    def string_to_number(value) -> int:
         '''
         получает строку в которой записано число возвращает целое число округленное int
         '''
-        return (int(float(value)))
+        return int(float(value))
